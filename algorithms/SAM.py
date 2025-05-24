1ca11a5edfb8cea6e73c87c1a7406d5e485f7c83
Leonardo Lamanna <leonardo.lamanna.75@gmail.com>
2025-05-24 21:32:50 +0200

    add baselines and syntactic metrics

---
+++ algorithms/SAM.py
@@ -0,0 +1,64 @@
+import sys
+import os
+sys.path.append(os.path.abspath("algorithms/sam"))
+
+from algorithms.AlgorithmAdapter import AlgorithmAdapter
+from typing import List
+import shutil
+from pathlib import Path
+from pddl_plus_parser.lisp_parsers import DomainParser, TrajectoryParser
+
+from algorithms.sam.sam_learning.learners import SAMLearner
+
+
+class SAM(AlgorithmAdapter):
+
+    def __init__(self, **kwargs):
+        super(SAM, self).__init__(**kwargs)
+
+    def learn(self,
+              domain_file: str,
+              trajectory_files: List[str]) -> str:
+
+        # Format input trajectories
+        os.makedirs('tmp', exist_ok=True)
+        filled_traj_paths = []
+        for i, traj_path in enumerate(sorted(trajectory_files,
+                                             key=lambda x: int(x.split('/')[-1].split('_')[0]))):
+            filled_traj = self.preprocess_trace(traj_path)  # add relevant negative literals
+            filled_traj_paths.append(f"tmp/{i}_traj_filled")
+            with open(f"tmp/{i}_traj_filled", "w") as f:
+                f.write(filled_traj)
+
+        # Instantiate SAM algorithm
+        partial_domain = DomainParser(Path(domain_file), partial_parsing=True).parse_domain()
+        sam = SAMLearner(partial_domain=partial_domain)
+
+        # Parse input trajectories
+        allowed_observations = [TrajectoryParser(partial_domain).parse_trajectory(traj_path)
+                                for traj_path in sorted(filled_traj_paths,
+                                                        key=lambda x: int(x.split('/')[-1].split('_')[0]))]
+
+        # Learn action model
+        learned_model, learning_report = sam.learn_action_model(allowed_observations)
+
+        # Remove temporary files
+        shutil.rmtree('tmp')
+
+        return learned_model.to_pddl()
+
+    def preprocess_trace(self, traj_path: str) -> str:
+
+        with open(traj_path, 'r') as f:
+            traj_str = f.read()
+
+        # Remove initial 'observation' keyword
+        traj_str = traj_str.replace('(observation', '(')
+
+        # Rename `action` into `operator`
+        traj_str = traj_str.replace('(:action ', '(operator: ')
+
+        # Rename the first state into `init`
+        traj_str = traj_str.replace('(:state ', '(:init ', 1)
+
+        return traj_str
