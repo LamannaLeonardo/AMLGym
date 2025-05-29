5f08e75fd752217063499aa6dd0ed775b20bc1b5
Leonardo Lamanna <leonardo.lamanna.75@gmail.com>
2025-05-29 09:22:49 +0200

    add results and dataset statistics

--- util/stats_gen.py
+++ util/stats_gen.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import numpy as np
 from alive_progress import alive_bar
 
 # Add current project to sys path
@@ -36,6 +37,11 @@ def write_trajectory_stats(file_name):
     all_domains_stats = dict()
     domains = sorted([d for d in os.listdir(f"../{BENCHMARK_DIR}/{TRAJ_DIR}")
                       if d not in ['.DS_Store']])
+
+    min_objs = np.inf
+    max_objs = -1
+    min_states = np.inf
+    max_states = -1
     with alive_bar(len(domains),
                    title=f'Producing trajectory statistics',
                    length=20,
@@ -69,6 +75,12 @@ def write_trajectory_stats(file_name):
             stats = pd.DataFrame(all_traj_stats)
             all_domains_stats[domain] = stats
 
+            # Update overall stats
+            min_objs = min(min_objs, all_domains_stats[domain]['objects'].min())
+            max_objs = max(max_objs, all_domains_stats[domain]['objects'].max())
+            min_states = min(min_states, all_domains_stats[domain]['states'].min())
+            max_states = max(max_states, all_domains_stats[domain]['states'].max())
+
             bar()  # update progress bar
 
     # Write all domains statistics into xlsx
@@ -88,7 +100,6 @@ def write_trajectory_stats(file_name):
     # Write one sheet for each domain
     for name, stats in all_domains_stats.items():
         stats.to_excel(writer, sheet_name=name, index=False, float_format="%.2f")
-        # Access the worksheet
 
         # Auto-adjust the column width to make headers fully readable
         worksheet = writer.sheets[name]
@@ -100,6 +111,12 @@ def write_trajectory_stats(file_name):
             ) + 2  # offset
             worksheet.set_column(col_num, col_num, max_len)
     writer.close()
+
+    # print overall stats
+    print(f'Minimum number of objects: {min_objs}\n'
+          f'Maximum number of objects: {max_objs}\n'
+          f'Minimum number of states: {min_states}\n'
+          f'Maximum number of states: {max_states}')
 
 
 def write_domain_stats(file_name):
@@ -128,6 +145,12 @@ def write_domain_stats(file_name):
                 'min predicates arity': min(f.arity for f in upl_domain.fluents),
                 'max predicates arity': max(f.arity for f in upl_domain.fluents),
                 'types': len(upl_domain.user_types),
+                'constants': 'no' if domain not in ['childsnack'] else 'yes',
+                'negative preconditions': 'no',
+                'injective assumption': 'no' if np.any([len([p.type.name for p in a.parameters])
+                                                    > len({p.type.name for p in a.parameters})
+                                                    for a in upl_domain.actions]) else 'yes'
+
             }
 
             # Store domain stats
@@ -196,11 +219,49 @@ def plot_avg_attr(df_file_path: str,
     grouped.plot(kind='bar', color='skyblue', edgecolor='none')
     # plt.title('Average number of objects')
     plt.xlabel('Trajectory', size=18)
-    plt.ylabel(f'Avg #{attribute}', size=18)
+    plt.ylabel(f'#{attribute.capitalize()}', size=18)
     plt.xticks(rotation=0, size=13)
     plt.yticks(rotation=0, size=13)
     plt.tight_layout()
     plt.savefig(img_file_path)
+
+
+def print_domain_stats(domain_df):
+    df = pd.read_excel(f"../{BENCHMARK_DIR}/domains.xlsx")
+    print(f"Max operators: {df['operators'].max()}")
+    print(f"min operators: {df['operators'].min()}")
+    print(f"Max predicates: {df['predicates'].max()}")
+    print(f"min predicates: {df['predicates'].min()}")
+    print(f"Max types: {df['types'].max()}")
+    print(f"min types: {df['types'].min()}")
+
+
+def print_traj_table(traj_df):
+    # Load Excel file with multiple sheets
+    xls = pd.read_excel(traj_df, sheet_name=None)  # sheet_name=None loads all sheets
+
+    # Identify common columns
+    common_cols = set.intersection(*(set(df.columns) for df in xls.values()))
+
+    # Compute averages for common columns per sheet
+    averages = {
+        sheet_name: df[list(common_cols)].mean(numeric_only=True)
+        for sheet_name, df in xls.items()
+    }
+
+    # Convert to DataFrame for LaTeX export
+    average_df = pd.DataFrame(averages).T  # Transpose to have sheet names as rows
+
+    # Optionally round for presentation
+    average_df = average_df.round(2)
+
+    # Export to LaTeX
+    latex_table = average_df.to_latex(index=True,
+                                      caption="Stastistics of the trajectories.",
+                                      label="tab:traj-stats",
+                                      float_format="%.2f")
+
+    print(latex_table)
 
 
 if __name__ == '__main__':
@@ -209,30 +270,38 @@ if __name__ == '__main__':
     # logging.basicConfig(level=logging.DEBUG)
     BENCHMARK_DIR = "benchmarks"
     DOMAINS_DIR = "domains"
-    PROB_DIR = "problems/trajectories"
-    TRAJ_DIR = "trajectories"
+    PROB_DIR = "problems/learning"
+    TRAJ_DIR = "trajectories/learning"
 
-    # Generate xlsx file of trajectory statistics
-    write_trajectory_stats(f"../{BENCHMARK_DIR}/trajectories.xlsx")
+    # # Generate xlsx file of trajectory statistics
+    # write_trajectory_stats(f"../{BENCHMARK_DIR}/trajectories_hard.xlsx")
+    # print_traj_table(f"../{BENCHMARK_DIR}/trajectories_hard.xlsx")
+    #
+    # # Generate xlsx file of domain statistics
+    # write_domain_stats(f"../{BENCHMARK_DIR}/domains.xlsx")
+    #
+    # # # Print domain statistics into a markdown table
+    # domain_table_columns = ['domain', 'operators', 'predicates', 'types', 'constants',
+    #                         'min operators arity', 'max operators arity',
+    #                         'min predicates arity', 'max predicates arity', 'negative preconditions',
+    #                         'injective assumption']
+    # print(pd.read_excel(f"../{BENCHMARK_DIR}/domains.xlsx")[domain_table_columns].to_markdown(index=False))
+    #
+    # # Print domain statistics into an html table
+    # print(pd.read_excel(f"../{BENCHMARK_DIR}/domains.xlsx")[domain_table_columns].to_html(index=False))
+    #
+    # # Print domain statistics into latex table
+    # print(pd.read_excel(f"../{BENCHMARK_DIR}/domains.xlsx")[domain_table_columns].to_latex(index=False))
 
-    # Generate xlsx file of domain statistics
-    write_domain_stats(f"../{BENCHMARK_DIR}/domains.xlsx")
+    # # Print domain statistics
+    # print_domain_stats(pd.read_excel(f"../{BENCHMARK_DIR}/domains.xlsx"))
 
-    # Print domain statistics into a markdown table
-    domain_table_columns = ['domain', 'operators', 'predicates', 'types',
-                            'max operators arity', 'max predicates arity',
-                            'min operators arity', 'min predicates arity']
-    print(pd.read_excel(f"../{BENCHMARK_DIR}/domains.xlsx")[domain_table_columns].to_markdown(index=False))
-
-    # Print domain statistics into an html table
-    print(pd.read_excel(f"../{BENCHMARK_DIR}/domains.xlsx")[domain_table_columns].to_html(index=False))
-
-    # Plot number of objects for every trace averaged over all domains
-    plot_avg_attr(f"../{BENCHMARK_DIR}/trajectories.xlsx",
-                  'objects',
-                  f"../{BENCHMARK_DIR}/objects.png")
-
-    # Plot number of states for every trace averaged over all domains
-    plot_avg_attr(f"../{BENCHMARK_DIR}/trajectories.xlsx",
-                  'states',
-                  f"../{BENCHMARK_DIR}/states.png")
+    # # Plot number of objects for every trace averaged over all domains
+    # plot_avg_attr(f"../{BENCHMARK_DIR}/trajectories_hard.xlsx",
+    #               'objects',
+    #               f"../{BENCHMARK_DIR}/objects.png")
+    #
+    # # Plot number of states for every trace averaged over all domains
+    # plot_avg_attr(f"../{BENCHMARK_DIR}/trajectories_hard.xlsx",
+    #               'states',
+    #               f"../{BENCHMARK_DIR}/states.png")
