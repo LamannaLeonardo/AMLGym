54692c88cb5cd243531ab68460ea80404de882e1
Leonardo Lamanna <leonardo.lamanna.75@gmail.com>
2025-10-24 11:40:27 +0200

    add local test command

--- scripts/make_distrib.sh
+++ scripts/make_distrib.sh
@@ -1,3 +1,3 @@
-#which python
 #python -m build  # locally build distribution
+#pip install dist/aml_evaluation-1.0.0-py3-none-any.whl  # test locally
 #python -m twine upload dist/*  # upload local distribution to PyPI
