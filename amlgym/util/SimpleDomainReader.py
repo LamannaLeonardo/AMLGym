793a9b2d1fb669dfbe401740c1dab5091f266c66
Leonardo Lamanna <leonardo.lamanna.75@gmail.com>
2025-12-19 21:28:33 +0100

    format space

--- amlgym/util/SimpleDomainReader.py
+++ amlgym/util/SimpleDomainReader.py
@@ -96,7 +96,7 @@ class SimpleDomainReader:
         with open(f_name, 'r') as f:
             data = f.read().split("\n")
 
-            objects_row = [el.replace(")","").strip()
+            objects_row = [el.replace(")", "").strip()
                            for el in re.findall(":types.*\(:predicates","++".join(data))[0].replace(":types","").replace("(:predicates", "").split("++")
                            if el.strip() != ""]
 
