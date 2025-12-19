5081f9d0d6543e4b7fb1fa2bc70392b3578e7d8b
Leonardo Lamanna <leonardo.lamanna.75@gmail.com>
2025-12-19 21:24:50 +0100

    format space and omit predictive_power function

--- amlgym/metrics/__init__.py
+++ amlgym/metrics/__init__.py
@@ -1,11 +1,15 @@
 from ._syntactic import syntactic_precision, syntactic_recall
 from ._solving import problem_solving
+from ._predictive import predictive_power, predicted_effects, applicability
 
 __all__ = [
     "print_metrics",
     "syntactic_precision",
     "syntactic_recall",
     "problem_solving",
+    "predictive_power",
+    "predicted_effects",
+    "applicability",
 ]
 
 
@@ -15,7 +19,8 @@ def print_metrics() -> None:
 
     :return:
     """
-    metrics = [name for name in __all__ if name != "print_metrics"]
+    metrics = [name for name in __all__
+               if name not in ["print_metrics", "predictive_power"]]
     print("Available metrics:")
     for m in metrics:
         print(f" - {m}")
