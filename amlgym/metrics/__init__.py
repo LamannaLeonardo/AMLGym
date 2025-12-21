eae36d4530143e34fda4ad7c1da6c634b3504c45
Leonardo Lamanna <leonardo.lamanna.75@gmail.com>
2025-12-21 11:01:19 +0100

    remove lefover merge markers

--- amlgym/metrics/__init__.py
+++ amlgym/metrics/__init__.py
@@ -1,21 +1,16 @@
 from ._syntactic import syntactic_precision, syntactic_recall
 from ._solving import problem_solving
-<<<<<<< HEAD
 from ._predictive import predictive_power, predicted_effects, applicability
-=======
->>>>>>> origin/main
+
 
 __all__ = [
     "print_metrics",
     "syntactic_precision",
     "syntactic_recall",
     "problem_solving",
-<<<<<<< HEAD
     "predictive_power",
     "predicted_effects",
     "applicability",
-=======
->>>>>>> origin/main
 ]
 
 
@@ -25,12 +20,8 @@ def print_metrics() -> None:
 
     :return:
     """
-<<<<<<< HEAD
     metrics = [name for name in __all__
                if name not in ["print_metrics", "predictive_power"]]
-=======
-    metrics = [name for name in __all__ if name != "print_metrics"]
->>>>>>> origin/main
     print("Available metrics:")
     for m in metrics:
         print(f" - {m}")
